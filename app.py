ca46c6af08c964c8bea8682498728a188bcff1e9
Estefanía Turín <85754386+estefiturin@users.noreply.github.com>
2022-01-12 00:26:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 from helper import pets
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('home.html')
   return '''
     <h1>Adopt a Pet!</h1>
       <p> Browse through the links bewlow to find your new furry friend </p>
@@ -15,8 +14,6 @@ def index():
         <li><a href="/animals/rabbits">Rabbits</a></li>
       </ul>
     '''
-if __name__ == '__main__':
-     app.run()
 
 @app.route('/animals/<pet_type>')
 def animals(pet_type):
